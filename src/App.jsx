4eeeed2db8086400e7ee089b7c64f3293b295e03
Danilo Almeida <danilo.laet52@gmail.com>
2025-02-06 19:51:35 -0300

    pequenas mudanças nas rotas e indentação

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Header from './Componets/Header/Header.jsx'
 
-
-
 export default function App(){
   return(
     <section>
       <Header />
       <h1>APP</h1>
     </section>
-  )
+  );
 }
--- src/Componets/Header/Header.jsx
+++ src/Componets/Header/Header.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import s from "./header.module.scss";
-import Inicio from "../../pages/Sobre.jsx";
+import Inicio from "../../pages/Sobre/Sobre.jsx";
 import ftdan from "../../assets/ftdan.jpg";
-import Projetos from "../../pages/Projetos.jsx";
-import Contato from "../../pages/Contato.jsx";
-import Skills from "../../pages/Skills.jsx";
+import Projetos from "../../pages/Projetos/Projetos.jsx";
+import Contato from "../../pages/Contato/Contato.jsx";
+import Skills from "../../pages/Skill/Skills.jsx";
 
 export default function Header() {
   return (
@@ -14,8 +14,6 @@ export default function Header() {
           <img src={ftdan} alt="Imagem de perfil da pessoal fisica Danilo" />
           <h1>Danilo Almeida</h1>
         </section>
-
-        
           <nav className={s.navHeader}>
             <ul>
               <li>
