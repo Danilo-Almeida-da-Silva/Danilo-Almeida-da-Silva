ad56dbf32254726bce0d12013628b431c0da418f
Danilo Almeida <danilo.laet52@gmail.com>
2025-02-18 20:14:20 -0300

    adição de imagens e outras informações das page skills

--- src/pages/Skill/Skills.jsx
+++ src/pages/Skill/Skills.jsx
@@ -3,9 +3,9 @@ import s from '../Skill/Skills.module.scss'
 import icon_python from '../../assets/logopython.png'
 import htmlicon from '../../assets/HTML5.png'
 import reactjs from '../../assets/Reactjs.png'
-import sass from '../../assets/Sassicon.png'
 import css from '../../assets/CSS3Logo.jfif'
 import js from '../../assets/javaScript.jfif'
+import sqlbd from '../../assets/SQL.png'
 
 export default function Skills() {
 return (
@@ -15,25 +15,26 @@ return (
                 <h2>Minhas Skills</h2>
             </section>
             <section className={s.iconskill}>
-                <ul>
-                    <li>
+                <ul className={s.listaskill}>
+                    <li className={s.lipython}>
                         <img src={icon_python} alt="logo tipo da linguagem de programação Python" />
                         <p>Python</p>
                     </li>
                     <li>
                         <img src={htmlicon} alt="" />
+                        <p>HTML</p>
                     </li>
                     <li>
                         <img src={reactjs} alt="" />
-                    </li>
-                    <li>
-                        <a href={sass}></a>
-                    </li>
-                    <li>
-                        <img src={css} alt="" />
+                        Reactjs
                     </li>
                     <li>
                         <img src={js} alt="" />
+                        <p>javaScript</p>
+                    </li>
+                    <li>
+                        <img src={css} alt="" />
+                        <p>CSS</p>
                     </li>
                     
                 </ul>
