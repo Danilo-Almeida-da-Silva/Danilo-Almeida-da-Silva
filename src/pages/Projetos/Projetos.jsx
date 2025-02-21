ec98ab8c52b85aa741fd414954a43fecd450065b
Danilo Almeida <danilo.laet52@gmail.com>
2025-02-20 23:22:18 -0300

    adição de informações a page projetos

--- src/pages/Projetos/Projetos.jsx
+++ src/pages/Projetos/Projetos.jsx
@@ -1,6 +1,42 @@
-export default function Projetos(){
-    return(
-        <h1>Projetos</h1>
+import React from "react";
+import s from '../Projetos/Projetos.module.scss'
+import projeto1 from '../../assets/projetonoite-react-vnw-dh6n-vercel-app.jpg'
+import projeto2 from '../../assets/projeto-outubro-rosa-vercel-app.jpg'
 
+
+export default function Projetos(){
+    const projetosprontos =[
+        {
+            nome: 'Doação de livro',
+            imagem: projeto1,
+            url: 'https://projetonoite-react-vnw-bgp3.vercel.app/'
+        },
+        {
+            nome: 'Outubro rosa 2',
+            imagem: projeto2,
+            url: 'https://projeto-outubro-rosa.vercel.app/'
+
+        },
+    ];
+    return(
+        <main>
+            <section className={s.secaoprojetos}>
+                    <h1 className={s.tituloprojetos}>Meus Projetos</h1>
+                <section className={s.visuaprojetos}>
+                    {projetosprontos.map((projeto, index) => (
+                        <article key={index} className={s.cardprojeto}>
+                            <img 
+                                className={s.imagemprojeto} 
+                                src={projeto.imagem} 
+                                alt={projeto.nome} 
+                            />
+                            <h2 className={s.nomeprojeto}>{projeto.nome}</h2>
+                            <button onClick={() => window.open(projeto.url, "_blank")} 
+                            className={s.botaoprojeto}>Ver Projeto</button>
+                        </article>
+                    ))}
+                </section>
+            </section>
+        </main>
     )
 }
